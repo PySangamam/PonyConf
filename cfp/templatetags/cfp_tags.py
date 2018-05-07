a7818fd7c6e96e65279e19f1ff207c06f48989f1
Shrayas <shrayasr@gmail.com>
2018-05-07 12:27:17 +0530

    Call markdown on the talk proposal

--- cfp/templatetags/cfp_tags.py
+++ cfp/templatetags/cfp_tags.py
@@ -1,9 +1,15 @@
 from django import template
 
 from cfp.utils import is_staff
+from ponyconf.utils import markdown_to_html
 
 
 register = template.Library()
+
+
+@register.simple_tag
+def markdown(value):
+    return markdown_to_html(value)
 
 
 @register.filter
